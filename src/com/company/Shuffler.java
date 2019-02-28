5df88ccaf4ff8a7c57253f1cdc294097efe9a6de
BT_1N3_07 <rhu4100@bths.edu>
2019-02-28 08:39:29 -0500

    sa

--- src/com/company/Shuffler.java
+++ src/com/company/Shuffler.java
@@ -3,9 +3,7 @@ package com.company;
 public class Shuffler
 {
     private static final int SHUFFLE_COUNT = 1;
-    private static final int VALUE_COUNT = 10;
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Results of " + SHUFFLE_COUNT + " consecutive perfect shuffles:");
         int[] values1 = {0, 1, 2, 3};
         for (int j = 1; j <= SHUFFLE_COUNT; j++)
@@ -19,14 +17,13 @@ public class Shuffler
             System.out.println();
         }
         System.out.println();
+
         System.out.println("Results of " + SHUFFLE_COUNT + " consecutive efficient selection shuffles:");
         int[] values2 = {0, 1, 2, 3};
-        for (int j = 1; j <= SHUFFLE_COUNT; j++)
-        {
+        for (int j = 1; j <= SHUFFLE_COUNT; j++) {
             selectionShuffle(values2);
             System.out.print("  " + j + ":");
-            for (int k = 0; k < values2.length; k++)
-            {
+            for (int k = 0; k < values2.length; k++) {
                 System.out.print(" " + values2[k]);
             }
             System.out.println();
@@ -35,35 +32,15 @@ public class Shuffler
     }
     public static void perfectShuffle(int[] values)
     {
-        int[] cards = new int[ VALUE_COUNT / 2 ];
-        int[] shuffled = new int[ VALUE_COUNT - VALUE_COUNT / 2 ];
-        for( int i = 0; i < VALUE_COUNT / 2; i++ )
+        int[] shuffled = new int[values.length];
+        int k = 0;
+        for (int j = 0; j < values.length/2; j++)
         {
-            cards[i] = values[i];
-        }
-
-        for( int i = 0; i < VALUE_COUNT - VALUE_COUNT / 2; i++ )
-        {
-            shuffled[i] = values[ i + VALUE_COUNT / 2 ];
-        }
-        for( int i = 0; i < VALUE_COUNT / 2; i++ )
-        {
-            values[ 2 * i ] = shuffled[i];
-            values[ 2 * i + 1 ] = cards[i];
-        }
-        if( VALUE_COUNT % 2 != 0 )
-        {
-            values[ VALUE_COUNT - 1 ] = shuffled[ VALUE_COUNT - VALUE_COUNT / 2 ];
+            
         }
     }
     public static void selectionShuffle(int[] values)
     {
-        for(int k = VALUE_COUNT - 1; k >= 0; k--)
-        {
-            int r = (int)(Math.random() * k);
-            int tmp = values[r];
-            values[r] = values[k];
-            values[k] = tmp;
-        }
+
     }
 }
